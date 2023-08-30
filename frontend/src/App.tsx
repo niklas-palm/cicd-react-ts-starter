6bdc962fe868c50da769760983658ba4e4212fae
Gideon Landeman <gideonlandeman@gmail.com>
2023-08-30 14:25:24 +0200

    fix: restore React project to default template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Let's Learn React and Github Actions
+                    Learn React
                 </a>
             </header>
         </div>
